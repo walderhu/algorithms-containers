0b6c07fa6ccf5df8e3bc99119585dbd5551f2d89
denis | walderhu <denis.lipatov163@gmail.com>
2024-09-16 00:45:33 +0400

    del

--- src/Core/BinaryTree/s21_BinaryTree_core.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef __S21_BINARY_TREE_CORE_HPP__
-#define __S21_BINARY_TREE_CORE_HPP__
-
-#include "s21_BinaryTree.hpp"
-
-#endif  // __S21_BINARY_TREE_CORE_HPP__
--- src/Core/s21_containers.hpp
+++ src/Core/s21_containers.hpp
@@ -1,7 +1,6 @@
 #ifndef __S21_CONTAINER_HXX__
 #define __S21_CONTAINER_HXX__
 
-#include "BinaryTree/s21_BinaryTree.hpp"
 #include "Deque/s21_deque.hpp"
 #include "List/s21_list.hpp"
 #include "Multyset/s21_multyset.hpp"
