ef3b8f6510b8f3016ff412d36a124cbacf6a944d
denis | walderhu <denis.lipatov163@gmail.com>
2024-09-16 23:38:14 +0400

    пытаемся реализовать наследование

--- src/Core/Multiset/s21_multiset.hpp
+++ src/Core/Multiset/s21_multiset.hpp
@@ -3,14 +3,9 @@
 
 #include "../Set/s21_set.hpp"
 namespace s21 {
-/*
-получается если я сделаю инсерт виртуальным методом и сделаю перегрузку
-то вся остальная логика у меня заработает как надо, поскольку
-при заполнении и оставшееся ** у меня будет делегирование
-идти уже на переопределенную функцию
-*/
+
 template <class Key>
-class multiset : public s21::set<Key> {
+class multiset final : public s21::set<Key> {
  public:
   using Node = typename s21::set<Key>::Node;
 
@@ -22,47 +17,17 @@ class multiset : public s21::set<Key> {
   using const_iterator = typename s21::set<Key>::ConstIterator;
   using size_type = size_t;
 
-  multiset() noexcept : set<Key>::set() {};
-  multiset(std::initializer_list<Key> const &items) noexcept
-      : set<Key>::set(items) {}
-  multiset(const multiset &s) : set<Key>::set(s) {}
-  multiset(multiset &&s) : set<Key>::set(std::move(s)) {}
-  ~multiset() noexcept { set<Key>::clear(); }
-  multiset<Key> &operator=(multiset &&s) noexcept {
-    return set<Key>::operator=(std::move(s));
-  }
-  multiset<Key> &operator=(const multiset &s) noexcept {
-    return set<Key>::operator=(s);
-  }
+  multiset() noexcept;
+  multiset(std::initializer_list<Key> const &items) noexcept;
+  multiset(const multiset &s);
+  multiset(multiset &&s);
+  ~multiset() noexcept;
+  multiset<Key> &operator=(multiset &&s) noexcept;
+  multiset<Key> &operator=(const multiset &s) noexcept;
 
-  //   iterator find(const Key &key);
-  //   bool contains(const Key &key);
-
-  //   bool empty() const;
-  //   void clear();
-
-  //   std::pair<iterator, bool> insert(value_type value);
-  //   size_type max_size();
-
-  //   iterator begin();
-  //   iterator end();
-  //   ConstIterator cbegin() const;
-  //   ConstIterator cend() const;
-  //   void swap(multiset &other);
-  //   void erase(iterator pos);
-  //   void merge(multiset &other);
-  //   size_type size() const;
-
-  //   template <typename... Args>
-  //   std::vector<std::pair<iterator, bool>> insert_many(Args &&...args);
-
-  //  protected:
-  //   size_type _size;
+ protected:
   std::pair<iterator, bool> insert(value_type value, Node *&current,
                                    Node *parent);
-  //   void clear(Node *&current);
-  //   Node *deleteNode(Node *current, value_type key);
-  //   Node *minValueNode(Node *node);
 };
 
 }  // namespace s21
--- src/Core/Multiset/s21_multiset_core.hpp
+++ src/Core/Multiset/s21_multiset_core.hpp
@@ -4,11 +4,46 @@
 #include <iostream>
 
 #include "s21_multiset.hpp"
+
+template <class value_type>
+inline s21::multiset<value_type>::multiset() noexcept
+    : set<value_type>::set(){};
+
+template <class value_type>
+inline s21::multiset<value_type>::multiset(
+    std::initializer_list<value_type> const &items) noexcept
+    : set<value_type>::set(items) {}
+
+template <class value_type>
+inline s21::multiset<value_type>::multiset(const multiset &s)
+    : set<value_type>::set(s) {}
+
+template <class value_type>
+inline s21::multiset<value_type>::multiset(multiset &&s)
+    : set<value_type>::set(std::move(s)) {}
+
+template <class value_type>
+inline s21::multiset<value_type>::~multiset() noexcept {
+  set<value_type>::clear();
+}
+
+template <class value_type>
+inline auto s21::multiset<value_type>::operator=(multiset &&s) noexcept
+    -> multiset<value_type> & {
+  return set<value_type>::operator=(std::move(s));
+}
+
+template <class value_type>
+inline auto s21::multiset<value_type>::operator=(const multiset &s) noexcept
+    -> multiset<value_type> & {
+  return set<value_type>::operator=(s);
+}
+
 template <class value_type>
 inline auto s21::multiset<value_type>::insert(value_type value, Node *&current,
                                               Node *parent)
     -> std::pair<iterator, bool> {
-  std::cout << "hello" << std::endl;
+  std::cout << "hello" << std::endl;  // delete
   if (current == nullptr) {
     current = new Node(value);
     current->parent = parent;
