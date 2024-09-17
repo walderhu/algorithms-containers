aba15115126651540f5f432221d0e533cb8d1591
denis | walderhu <denis.lipatov163@gmail.com>
2024-09-17 22:10:14 +0400

    донастроил мейк

--- src/Core/main.cpp
+++ src/Core/main.cpp
@@ -4,23 +4,4 @@
 #include <set>
 
 #include "s21_containers.hpp"
-using namespace std;
-int main() {
-  s21::multiset<int> _multiset({5, 4, 1, 0, 2, 1, 4, 3, 3, 2});
-  auto i = _multiset.find(5);
-
-  // for (auto it = _multiset.begin(); it != _multiset.end(); ++it)
-  // cout << *it << " ";
-  // cout << endl;
-
-  _multiset.erase(i);
-
-  for (auto it = _multiset.begin(); it != _multiset.end(); ++it)
-    cout << *it << " ";
-  cout << endl;
-  cout << _multiset.contains(5) << endl;
-
-  return 0;
-}
-
-// BinaryTree
+int main() { return 0; }
