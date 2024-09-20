ff8510276c0f41c41420fc734404578298e855d8
denis | walderhu <denis.lipatov163@gmail.com>
2024-09-21 00:40:50 +0400

    добавил базовый функционал для своего множества

--- src/Core/UnorderedSet/s21_unordered_set.hpp
+++ src/Core/UnorderedSet/s21_unordered_set.hpp
@@ -3,35 +3,33 @@
 
 #include "../s21_containers.hpp"
 namespace s21 {
-
 template <class Key>
-class unordered_set final : public s21::set<Key> {
+class unordered_set {
  public:
-  using Node = typename set<Key>::Node;
-
   using key_type = Key;
   using value_type = Key;
   using reference = value_type &;
   using const_reference = const value_type &;
   using size_type = size_t;
-  using iterator = typename set<Key>::Iterator;
-  using const_iterator = typename set<Key>::ConstIterator;
 
   unordered_set() noexcept;
   unordered_set(std::initializer_list<Key> const &items) noexcept;
   unordered_set(const unordered_set &s) noexcept;
   unordered_set(unordered_set &&s) noexcept;
-  ~unordered_set() noexcept override;
+  ~unordered_set() noexcept;
   unordered_set<Key> &operator=(unordered_set &&s) noexcept;
   unordered_set<Key> &operator=(const unordered_set &s) noexcept;
 
   size_type hashFunction(key_type key);
+  void clear();
+  void remove(key_type key);
+  void insert(const key_type &key) noexcept;
+  bool contains(const key_type &key) const noexcept;
 
  protected:
   static constexpr size_type TABLE_SIZE = 100;
   mutable s21::vector<value_type> table[TABLE_SIZE];
 };
-
 }  // namespace s21
 #include "s21_unordered_set_core.hpp"
 
--- src/Core/UnorderedSet/s21_unordered_set_core.hpp
+++ src/Core/UnorderedSet/s21_unordered_set_core.hpp
@@ -6,46 +6,36 @@
 namespace s21 {
 
 template <class value_type>
-inline unordered_set<value_type>::unordered_set() noexcept
-    : set<value_type>::set(){};
+inline unordered_set<value_type>::unordered_set() noexcept {};
 
 template <class value_type>
 inline unordered_set<value_type>::unordered_set(
     std::initializer_list<value_type> const &items) noexcept {
-  for (auto it = items.begin(); it != items.end(); ++it)
-    set<value_type>::insert(*it);
+  for (auto it = items.begin(); it != items.end(); ++it) this->insert(*it);
 }
 
 template <class value_type>
-inline unordered_set<value_type>::unordered_set(const unordered_set &s) noexcept
-    : set<value_type>::set(s) {}
+inline unordered_set<value_type>::unordered_set(
+    const unordered_set &s) noexcept {}
 
 template <class value_type>
-inline unordered_set<value_type>::unordered_set(unordered_set &&s) noexcept
-    : set<value_type>::set(std::move(s)) {}
+inline unordered_set<value_type>::unordered_set(unordered_set &&s) noexcept {}
 
 template <class value_type>
 inline unordered_set<value_type>::~unordered_set() noexcept {
-  set<value_type>::clear();
+  this->clear();
 }
 
 template <class value_type>
 inline auto unordered_set<value_type>::operator=(unordered_set &&s) noexcept
     -> unordered_set<value_type> & {
-  if (this != &s) {
-    unordered_set<value_type>::clear();
-    this->root = std::move(s.root);
-    this->_size = std::move(s._size);
-    s.root = nullptr;
-    s._size = 0u;
-  }
   return *this;
 }
 
 template <class value_type>
 inline auto unordered_set<value_type>::operator=(
     const unordered_set &s) noexcept -> unordered_set<value_type> & {
-  return set<value_type>::operator=(s);
+  return *this;
 }
 
 // here
@@ -53,6 +43,30 @@ inline auto unordered_set<value_type>::operator=(
 template <class value_type>
 inline auto unordered_set<value_type>::hashFunction(key_type key) -> size_type {
   return std::hash<key_type>()(key);
+}
+
+template <class Key>
+inline void unordered_set<Key>::insert(const key_type &key) noexcept {
+  size_type index = this->hashFunction(key) % TABLE_SIZE;
+  this->table[index].push_back(key);
+}
+
+template <class Key>
+inline void unordered_set<Key>::clear() {}
+
+template <class Key>
+inline bool unordered_set<Key>::contains(const key_type &key) const noexcept {
+  size_type index = this->hashFunction(key) % TABLE_SIZE;
+  for (auto &&element : table[index])
+    if (element == key) return true;
+  return false;
+}
+
+template <class Key>
+inline void unordered_set<Key>::remove(key_type key) {
+  size_t index = hashFunction(key) % TABLE_SIZE;
+  auto &vec = table[index];
+  vec.erase(std::remove(vec.begin(), vec.end(), key), vec.end());
 }
 
 }  // namespace s21
