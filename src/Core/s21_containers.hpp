97c071cb0190f164c2ae336246e9eb8b895a5ac7
denis | walderhu <denis.lipatov163@gmail.com>
2024-09-23 14:59:20 +0400

    мерж последних изменений walderhu

--- src/Core/s21_containers.hpp
+++ src/Core/s21_containers.hpp
@@ -3,23 +3,9 @@
 
 #include <cstddef>
 #include <initializer_list>
-#include <iostream>  // TODO убрать
 #include <limits>
 #include <stdexcept>
 #include <utility>
-
-#define DEBUG(x)                                                    \
-  std::cout << "DEBUG: " << #x << " = " << x << " (in " << __FILE__ \
-            << ", line " << __LINE__ << ")" << std::endl;
-
-#define RED_TEXT "\033[31m"
-#define RESET_TEXT "\033[0m"
-
-#define PRINT(x)                                                               \
-  std::cout << RED_TEXT << x << RESET_TEXT << " (in " << __FILE__ << ", line " \
-            << __LINE__ << ")" << std::endl;
-
-#define PING() PRINT("ping")
 
 namespace s21 {
 
@@ -36,12 +22,6 @@ template <class T>
 class multiset;
 
 template <class T>
-class unordered_set;
-
-template <class T>
-class unordered_multiset;
-
-template <class T>
 class vector;
 
 }  // namespace s21
@@ -50,8 +30,6 @@ class vector;
 #include "List/s21_list.hpp"
 #include "Multiset/s21_multiset.hpp"
 #include "Set/s21_set.hpp"
-#include "UnorderedMultiset/s21_unordered_multiset.hpp"
-#include "UnorderedSet/s21_unordered_set.hpp"
 #include "Vector/s21_vector.hpp"
 
 #endif  // __S21_CONTAINER_HXX__
