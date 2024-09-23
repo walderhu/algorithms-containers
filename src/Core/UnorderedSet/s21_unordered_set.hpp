c5eb0d25cfb8b25b18760f1f10a16053f4760970
denis | walderhu <denis.lipatov163@gmail.com>
2024-09-23 23:24:33 +0400

    небольшой рефакторинг пронстранства

--- src/Core/UnorderedSet/s21_unordered_set.hpp
+++ src/Core/UnorderedSet/s21_unordered_set.hpp
@@ -31,7 +31,6 @@ class unordered_set {
   size_type hashFunction(key_type key) const noexcept;
   void clear() noexcept;
   virtual std::pair<Iterator, bool> insert(const key_type &key) noexcept;
-  virtual void debug();  // TODO убрать
 
   Iterator begin() noexcept;
   Iterator end() noexcept;
@@ -52,71 +51,6 @@ class unordered_set {
   std::vector<std::pair<iterator, bool>> insert_many(Args &&...args);
   template <typename... Args>
   std::vector<std::pair<iterator, bool>> emplace_back(Args &&...args);
-  /*
-  Метод load_factor в классе std::unordered_set используется для получения
-  текущего коэффициента загрузки контейнера. Коэффициент загрузки — это
-  отношение количества элементов в контейнере к количеству ведер (buckets),
-  используемых для хранения этих элементов. Он позволяет оценить, насколько
-  эффективно распределены элементы по ведрам и может помочь в управлении
-  производительностью хэш-таблицы.
-
-  я сначала создаю 100 пустых ведер, но они пустые
-  считать их полноценно их ведрами можно только после того как там есть хотя бы
-  1 элемент в случае сток сета использую size потому что он полностью
-  семанимичен, в мультисете отдельный метод
-  */
-  /*
-    template <class... Args>
-    iterator emplace_hint(const_iterator position, Args &&...args);
-  итератор это подсказка, мы можем игнорировать его полностью, но не стоит
-  */
-  /*
-  void reserve(size_type n);
-  нужно для перераспределения памяти на более емкую чтоли???
-  просто переопределить более корректную table_max для такущей структуры
-  и переопределить ее
-  */
-  /*
-  Метод rehash в классе std::unordered_set используется для изменения количества
-  ведер (buckets) в контейнере. Он позволяет установить новое количество ведер,
-  что может быть полезно для оптимизации производительности при добавлении
-  элементов.
-  void rehash(size_type n);
-  в моем случае начальное значение этих ведер это table_max
-  */
-  /*
-  Метод key_eq в классе std::unordered_set возвращает функцию, которая
-  используется для сравнения ключей на равенство. Это позволяет пользователю
-  проверить, равны ли два ключа, используя определенный предикат сравнения,
-  который может быть задан при создании unordered_set.
-  key_equal key_eq() const;
-      auto cmpfn = mySet.key_eq();
-
-  Проверяем равенство двух символов
-    std::cout << "cmpfn('a', 'a') == " << std::boolalpha << cmpfn('a', 'a') <<
-  std::endl; // true std::cout << "cmpfn('a', 'b') == " << std::boolalpha <<
-  cmpfn('a', 'b') << std::endl; // false
-
-  */
-  /*
-  Метод get_allocator в классе std::unordered_set возвращает объект
-  распределителя (allocator), который используется для управления памятью в
-  контейнере. Это позволяет пользователям получить доступ к распределителю,
-  который был использован для выделения памяти для элементов в unordered_set.
-  allocator_type get_allocator() const noexcept;
-    std::unordered_set<int> mySet;
-
-    // Получаем распределитель
-    auto alloc = mySet.get_allocator();
-
-    // Выводим адреса выделенной памяти
-    int* p = alloc.allocate(1); // Выделяем память для одного элемента
-    *p = 42; // Присваиваем значение
-
-    std::cout << "Allocated memory for value: " << *p << std::endl;
-
-    alloc.deallocate(p, 1); // Освобождаем память
-  */
 
  protected:
   size_type get_index(const key_type &key) const noexcept;
@@ -128,6 +62,7 @@ class unordered_set {
   using IteratorType = typename s21::list<
       std::array<std::vector<value_type>, TABLE_SIZE> *>::iterator;
   IteratorType to_expand() noexcept;
+  virtual void debug();  // TODO убрать
 };
 
 }  // namespace s21
